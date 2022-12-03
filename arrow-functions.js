23bfbe5d622c4883f459145bd1b12ec0865ba783
AndypSheridan <andy.p.sheridan@gmail.com>
2022-12-03 16:21:44 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,9 +5,28 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
+
 // Arrow Function With Parameters
 
+// const addTwoNumbers = (a, b) => {
+//     return a + b
+// }
+
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
+
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers2 = (a, b) => a + b;
+let sum = addTwoNumbers2(3, 5);
+console.log(sum);
 
 // Implicit Returns
 
